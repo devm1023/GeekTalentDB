e27c5eeb23cc5ef851565a08de86901de4418c7b
devm1023 <devm1023@gmail.com>
2015-10-07 19:04:31 +0100

    re-organizes structure_linkedin.py

--- structure_linkedin.py
+++ structure_linkedin.py
@@ -4,96 +4,130 @@ import datoindb as dt
 import sys
 from datetime import datetime, timedelta
 from logger import Logger
-from windowquery import windowQuery
+from windowquery import windowQuery, partitions
+from parallelize import ParallelFunction
+from logger import Logger
 
 
 timestamp0 = datetime(year=1970, month=1, day=1)
-logger = Logger(sys.stdout)
-
-maxprofiles = 10
-windowsize = 5
 
 
-# connect to databases
+# process arguments
 
-gtdb = GeekTalentDB(url=conf.GT_WRITE_DB)
-dtdb = dt.DatoinDB(url=conf.DT_READ_DB)
-
-
-# compute timestamps
-
-fromdate = datetime.strptime(sys.argv[1], '%Y-%m-%d')
+njobs = int(sys.argv[1])
+fromdate = datetime.strptime(sys.argv[2], '%Y-%m-%d')
 if len(sys.argv) > 2:
-    todate = datetime.strptime(sys.argv[2], '%Y-%m-%d')
+    todate = datetime.strptime(sys.argv[3], '%Y-%m-%d')
 else:
     todate = datetime.now()
 
 fromTs = int((fromdate - timestamp0).total_seconds())*1000
 toTs   = int((todate   - timestamp0).total_seconds())*1000
 
-logger.log('fromTs: {0:d}\ntoTs:   {1:d}\n'.format(fromTs, toTs))
-
-profilecount = 0
-for dtprofile in windowQuery(
-        dtdb.query(dt.LIProfile) \
-            .filter(dt.LIProfile.indexedOn >= fromTs,
-                    dt.LIProfile.indexedOn < toTs),
-        dt.LIProfile.id,
-        windowsize=windowsize):
-    profilecount += 1
-
-    # get location
-    if dtprofile.country and dtprofile.city:
-        location = ', '.join([dtprofile.city, dtprofile.country])
-    elif not dtprofile.country and not dtprofile.city:
-        location = None
-    elif not dtprofile.country:
-        location = dtprofile.city
-    else:
-        location = dtprofile.country
-
-    # get experiences
-    experiences = []
-    for dtexperience in dtdb.query(dt.Experience) \
-                            .filter(dt.Experience.parentId == dtprofile.id):
-        # get start date
-        if not dtexperience.dateFrom:
-            startdate = None
-        else:
-            startdate \
-                = timestamp0 + timedelta(milliseconds=dtexperience.dateFrom)
-
-        # get end date
-        if not dtexperience.dateTo:
-            enddate = None
-        else:
-            enddate \
-                = timestamp0 + timedelta(milliseconds=dtexperience.dateTo)
-
-        experiences.append({'company'     : dtexperience.company,
-                            'title'       : dtexperience.name,
-                            'description' : dtexperience.description,
-                            'startdate'   : startdate,
-                            'enddate'     : enddate})
-
+def structureProfiles(fromTs, toTs, id1, id2):
+    windowsize = 100
+    logger = Logger(sys.stdout)
     
-    # add profile
-    gtdb.addLIProfile(dtprofile.parentId,
-                      dtprofile.name,
-                      dtprofile.title,
-                      dtprofile.description,
-                      location,
-                      dtprofile.profileUrl,
-                      dtprofile.profilePictureUrl,
-                      dtprofile.categories,
-                      experiences)
+    jobstart = datetime.now()
+    logger.log(jobstart.strftime('Starting computation %Y-%m-%d %H:%M:%S%z.\n'))
 
-    # commit
-    if profilecount % windowsize == 0:
-        logger.log('{0:d} profiles processed.\n'.format(profilecount))
-        gtdb.commit()
+    logger = Logger(sys.stdout)
+    gtdb = GeekTalentDB(url=conf.GT_WRITE_DB)
+    dtdb = dt.DatoinDB(url=conf.DT_READ_DB)
 
-    if profilecount >= maxprofiles:
-        break
-gtdb.commit()
+    q = dtdb.query(dt.LIProfile) \
+            .filter(dt.LIProfile.indexedOn >= fromTs, \
+                    dt.LIProfile.indexedOn < toTs)
+    if id1 is not None:
+        if id2 is not None:
+            q = q.filter(dt.LIProfile.id >= id1, dt.LIProfile.id < id2)
+        else:
+            q = q.filter(dt.LIProfile.id >= id1)
+        
+    profilecount = 0
+    for dtprofile in windowQuery(q, dt.LIProfile.id, windowsize=windowsize):
+        profilecount += 1
+
+        # get location
+        if dtprofile.country and dtprofile.city:
+            location = ', '.join([dtprofile.city, dtprofile.country])
+        elif not dtprofile.country and not dtprofile.city:
+            location = None
+        elif not dtprofile.country:
+            location = dtprofile.city
+        else:
+            location = dtprofile.country
+
+        # get experiences
+        experiences = []
+        for dtexperience in dtdb.query(dt.Experience) \
+                                .filter(dt.Experience.parentId == dtprofile.id):
+            # get start date
+            if not dtexperience.dateFrom:
+                startdate = None
+            else:
+                startdate \
+                    = timestamp0 + timedelta(milliseconds=dtexperience.dateFrom)
+
+            # get end date
+            if not dtexperience.dateTo:
+                enddate = None
+            else:
+                enddate \
+                    = timestamp0 + timedelta(milliseconds=dtexperience.dateTo)
+
+            experiences.append({'company'     : dtexperience.company,
+                                'title'       : dtexperience.name,
+                                'description' : dtexperience.description,
+                                'startdate'   : startdate,
+                                'enddate'     : enddate})
+
+        # remove truncated skills
+        skills = [s for s in dtprofile.categories \
+                  if len(s) < 4 or s[-3:] != '...']
+
+        # add profile
+        gtdb.addLIProfile(dtprofile.parentId,
+                          dtprofile.name,
+                          dtprofile.title,
+                          dtprofile.description,
+                          location,
+                          dtprofile.profileUrl,
+                          dtprofile.profilePictureUrl,
+                          skills,
+                          experiences)
+
+        # commit
+        if profilecount % windowsize == 0:
+            gtdb.commit()
+            now = datetime.now()
+            deltat = (now-jobstart).total_seconds()
+            logger.log('{0:d} profiles processed at {1:f} profiles/sec.\n' \
+                       .format(profilecount, profilecount/deltat))
+    gtdb.commit()
+    now = datetime.now()
+    deltat = (now-jobstart).total_seconds()
+    logger.log('{0:d} profiles processed at {1:f} profiles/sec.\n' \
+               .format(profilecount, profilecount/deltat))
+
+
+# make batches
+
+dtdb = dt.DatoinDB(url=conf.DT_READ_DB)
+batchsize = 10
+q = dtdb.query(dt.LIProfile.id) \
+        .filter(dt.LIProfile.indexedOn >= fromTs, \
+                dt.LIProfile.indexedOn < toTs)
+
+
+if njobs > 1:
+    args = [(fromTs, toTs)+p for p in partitions(q, dt.LIProfile.id, njobs)]
+    ParallelFunction(structureProfiles,
+                     batchsize=1,
+                     workdir='sjobs',
+                     prefix='listructure',
+                     log=sys.stdout,
+                     tries=1)(args)
+else:
+    structureProfiles(fromTs, toTs, None, None)
 
--- windowquery.py
+++ windowquery.py
@@ -1,5 +1,6 @@
 import sqlalchemy
 from sqlalchemy import and_, func, text
+import numpy as np
 
 def windows(q, column, windowsize):
     """Generate a series of WHERE clauses which break a given column into windows.
@@ -19,6 +20,38 @@ def windows(q, column, windowsize):
             yield and_(column >= start_id, column < end_id)
         yield column >= intervals[-1]
 
+
+def partitions(q, column, nbatches):
+    """Break a query into even-sized batches according to values in one column.
+
+    Args:
+      q (query object): The query to split.
+      column (Column object): The column on which to split.
+      nbatches (int): The number of batches.
+
+    Yields:
+      A tuples holding the lower (inclusive) and upper (exclusive)
+      bounds for the values of `column`. For the last tuple the upper bound
+      will be ``None``.
+
+    """
+    nbatches = max(1, nbatches)
+    q = q.from_self(column).distinct()
+    ntotal = q.count()
+    rows = np.linspace(1, ntotal, nbatches+1, dtype=int)
+    rows = list(map(repr, rows))
+    columnvals = q.add_columns(func.row_number().over(order_by=column) \
+                               .label('__rownum__')) \
+                  .from_self(column, '__rownum__') \
+                  .filter(text('__rownum__ IN ({0:s})' \
+                               .format(', '.join(rows)))) \
+                  .order_by('__rownum__') \
+                  .all()
+    for a, b in zip(columnvals[:-2], columnvals[1:-1]):
+        yield a[0], b[0]
+    yield (columnvals[-2][0], None)
+    
+
 def windowQuery(q, column, windowsize=1000, values=None):
     """"Break a query into windows on a given column.
 
@@ -32,7 +65,10 @@ def windowQuery(q, column, windowsize=1000, values=None):
         performance you can use a simpler query than `q` here, e.g. by removing
         joins. `values` must return the same values for `column` as `q`. Defaults
         to ``None``, in which case the values are obtained from `q`.
-        
+    
+    Yields:
+      The same rows that `q` would yield.
+
     """
 
     if values is None:
