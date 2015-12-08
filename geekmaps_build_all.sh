91e48df883e97d2289e9d583db27ddd4c33ed0a6
devm1023 <devm1023@gmail.com>
2015-12-08 10:52:17 +0000

    adds --sudo option to geekmaps_build_all.sh

--- geekmaps_build_all.sh
+++ geekmaps_build_all.sh
@@ -6,8 +6,14 @@ python3 initdb.py geekmaps || exit 1
 echo
 echo
 echo "Copying catalogues."
-echo "# pg_dump -t skill -t skill_word -t title -t title_word -t sector -t company -t company_word -t word -a analytics | psql geekmaps"
-pg_dump -t skill -t skill_word -t title -t title_word -t sector -t company -t company_word -t word -a analytics | psql geekmaps || exit 1
+if [ "$3" = "--sudo" ]
+then
+    echo "# sudo -u postgres pg_dump -t skill -t skill_word -t title -t title_word -t sector -t company -t company_word -t word -a analytics | sudo -u postgres psql geekmaps"
+    sudo su -c "sudo -u postgres pg_dump -t skill -t skill_word -t title -t title_word -t sector -t company -t company_word -t word -a analytics | sudo -u postgres psql geekmaps" || exit 1
+else
+    echo "# pg_dump -t skill -t skill_word -t title -t title_word -t sector -t company -t company_word -t word -a analytics | psql geekmaps"
+    pg_dump -t skill -t skill_word -t title -t title_word -t sector -t company -t company_word -t word -a analytics | psql geekmaps || exit 1
+fi
 echo
 echo
 echo "Computing NUTS codes."
