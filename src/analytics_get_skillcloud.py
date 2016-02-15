9a8072adc69e62fe56f9ab872c85c63312fcfef7
devm1023 <devm1023@gmail.com>
2016-02-15 18:48:57 +0000

    updates analytics_get_skillcloud.py and adds analytics_train_availabilitymodel.py

--- src/analytics_get_skillcloud.py
+++ src/analytics_get_skillcloud.py
@@ -5,6 +5,7 @@ from sqlalchemy import func, or_
 import sys
 import csv
 from logger import Logger
+import argparse
 
 
 def getScores(q, categoryNameCol, categoryCountCol, skillCountCol, nrecords):
@@ -40,91 +41,91 @@ def getScores(q, categoryNameCol, categoryCountCol, skillCountCol, nrecords):
     return scores, categories, categoryCount
 
 
-try:
-    querytype = sys.argv[1]
-    if querytype not in ['title', 'company', 'skill']:
-        raise ValueError('Invalid category string')
-    query = sys.argv[2]
-    filename = None
-    if len(sys.argv) > 3:
-        filename = sys.argv[3]
-except (ValueError, KeyError):
-    sys.stdout.write('usage: python3 analytics_get_skillclouds.py '
-                     '(title | company | skill) <query>\n')
-    sys.stdout.flush()
-    exit(1)
+def getSkillCloud(querytype, query):
+    andb = AnalyticsDB(conf.ANALYTICS_DB)
+    logger = Logger(sys.stdout)
 
-andb = AnalyticsDB(conf.ANALYTICS_DB)
-logger = Logger(sys.stdout)
-
-csvwriter = None
-csvfile = None
-if filename is not None:
-    csvfile = open(filename, 'w')
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(['skill', 'relevance'])
-
-if querytype == 'title':
-    nrecords = andb.query(Experience.id).count()
-    titles, words = andb.findEntities('title', query)
-    titles = [title[0] for title in titles]
-    q = andb.query(TitleSkill.nrmTitle,
-                   TitleSkill.nrmSkill,
-                   TitleSkill.experienceCount) \
-            .filter(TitleSkill.nrmTitle.in_(titles))
-    scores, categories, nmatches \
-        = getScores(q, Title.nrmName, Title.experienceCount,
-                    Skill.experienceCount, nrecords)
-        
-    for skill, skillName, score in scores:
-        logger.log('{0:> 6.1f}% {1:s}\n'.format(score*100, skillName))
-        if csvwriter is not None:
-            csvwriter.writerow([skillName, score])
-    logger.log('\nMatching records: {0:d}\n'.format(nmatches))
-    logger.log('\nMatched titles\n')
-    for category, count in categories:
-        logger.log('    {0:7d} {1:s}\n'.format(count, category))
-if querytype == 'company':
-    nrecords = andb.query(Experience.id).count()
-    companies, words = andb.findEntities('company', query)
-    companies = [company[0] for company in companies]
-    q = andb.query(CompanySkill.nrmCompany,
-                   CompanySkill.nrmSkill,
-                   CompanySkill.experienceCount) \
-            .filter(CompanySkill.nrmCompany.in_(companies))
-    scores, categories, nmatches \
-        = getScores(q, Company.nrmName, Company.experienceCount,
-                    Skill.experienceCount, nrecords)
-        
-    for skill, skillName, score in scores:
-        logger.log('{0:> 6.1f}% {1:s}\n'.format(score*100, skillName))
-        if csvwriter is not None:
-            csvwriter.writerow([skillName, score])
-    logger.log('\nMatching records: {0:d}\n'.format(nmatches))
-    logger.log('\nMatched companies\n')
-    for category, count in categories:
-        logger.log('    {0:7d} {1:s}\n'.format(count, category))
-if querytype == 'skill':
-    nrecords = andb.query(Experience.id).count()
-    skills, words = andb.findEntities('skill', query)
-    skills = [skill[0] for skill in skills]
-    print(skills)
-    q = andb.query(SkillSkill.nrmSkill1,
-                   SkillSkill.nrmSkill2,
-                   SkillSkill.experienceCount) \
-            .filter(SkillSkill.nrmSkill1.in_(skills))
-    scores, categories, nmatches \
-        = getScores(q, Skill.nrmName, Skill.experienceCount,
-                    Skill.experienceCount, nrecords)
-        
-    for skill, skillName, score in scores:
-        logger.log('{0:> 6.1f}% {1:s}\n'.format(score*100, skillName))
-        if csvwriter is not None:
-            csvwriter.writerow([skillName, score])
-    logger.log('\nMatching records: {0:d}\n'.format(nmatches))
-    logger.log('\nMatched skills\n')
-    for category, count in categories:
-        logger.log('    {0:7d} {1:s}\n'.format(count, category))
+    nrecords = andb.query(LIExperience.id).count()
+    if querytype == 'title':
+        matches, words = andb.findEntities('title', query)
+        q = andb.query(TitleSkill.nrmTitle,
+                       TitleSkill.nrmSkill,
+                       TitleSkill.experienceCount) \
+                .filter(TitleSkill.nrmTitle.in_(matches))
+        scores, categories, nmatches \
+            = getScores(q, Title.nrmName, Title.experienceCount,
+                        Skill.experienceCount, nrecords)
+    elif querytype == 'company':
+        matches, words = andb.findEntities('company', query)
+        matches = [match[0] for match in matches]
+        q = andb.query(CompanySkill.nrmCompany,
+                       CompanySkill.nrmSkill,
+                       CompanySkill.experienceCount) \
+                .filter(CompanySkill.nrmCompany.in_(matches))
+        scores, categories, nmatches \
+            = getScores(q, Company.nrmName, Company.experienceCount,
+                        Skill.experienceCount, nrecords)
+    elif querytype == 'skill':
+        matches, words = andb.findEntities('skill', query)
+        matches = [match[0] for match in matches]
+        q = andb.query(SkillSkill.nrmSkill1,
+                       SkillSkill.nrmSkill2,
+                       SkillSkill.experienceCount) \
+                .filter(SkillSkill.nrmSkill1.in_(matches))
+        scores, categories, nmatches \
+            = getScores(q, Skill.nrmName, Skill.experienceCount,
+                        Skill.experienceCount, nrecords)
+    else:
+        raise ValueError('Invalid query type.')
     
-if csvfile is not None:
-    csvfile.close()
+    result = {}
+    result['nrecords'] = nrecords
+    result['nmatches'] = nmatches
+    result['skills'] = []
+    for skill, skillName, score in scores:
+        result['skills'].append({'name'  : skillName,
+                                 'score' : score})
+    result['matches'] = []
+    for match, count in categories:
+        result['matches'].append({'name'  : category,
+                                  'count' : count})
+
+    return result
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('querytype',
+                        choices=['title', 'company', 'skill'],
+                        help='The type of skill cloud to generate.')
+    parser.add_argument('query',
+                        help='The search term')
+    parser.add_argument('-o',
+                        help='The name of the output file (CSV).')
+    args = parser.parse_args()
+
+    logger = Logger(sys.stdout)
+    result = getSkillCloud(args.querytype, args.query)
+
+    try:
+        csvwriter = None
+        csvfile = None
+        if filename is not None:
+            csvfile = open(filename, 'w')
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['skill', 'relevance'])
+
+        for skill in result['skills']:
+            logger.log('{0:> 6.1f}% {1:s}\n' \
+                       .format(skill['score']*100, skill['name']))
+            if csvwriter is not None:
+                csvwriter.writerow([skill['name'], skill['score']])
+        logger.log('\nMatching records: {0:d}\n'.format(result['nmatches']))
+        logger.log('\nMatched entities\n')
+        for match in result['matches']:
+            logger.log('    {0:7d} {1:s}\n' \
+                       .format(match['count'], match['name']))
+    finally:
+        if csvfile is not None:
+            csvfile.close()
+
