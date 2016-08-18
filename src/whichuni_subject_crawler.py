ea731e2be10eab51a7e86e0a136ff0bf9749cd94
devm1023 <devm1023@gmail.com>
2016-08-18 10:24:38 +0100

    add whichuni subject crawler

---
+++ src/whichuni_subject_crawler.py
@@ -0,0 +1,30 @@
+from crawler import Crawler
+import re
+
+class WhichUniSubjectCrawler(Crawler):
+    """Crawler class for whichuni categories (fields of study)
+
+    Args:
+      site (str, optional): String ID of the crawled webpage. Defaults to
+        ``'whichunisubjects'``.
+      **kwargs (optional): All other keyword arguments are passed to the
+        constructor of ``crawler.Crawler``.
+
+    """
+
+    def __init__(self, site='whichunisubjects', **kwargs):
+        Crawler.__init__(self, site, **kwargs)
+    
+    @classmethod
+    def parse(cls, site, url, redirect_url, doc):
+        valid = False
+        links = []
+        page_type = 'alevel-explorer'
+        if doc is None:
+            return False, page_type, []
+        title_elem = doc.xpath('/html/head/title')
+        if title_elem:
+            valid = True
+        return valid, page_type, links
+        
+        
