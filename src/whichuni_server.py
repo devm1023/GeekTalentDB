32a54c86dc9b6b777b6a6e5dd0a7844eaa672a96
devm1023 <devm1023@gmail.com>
2016-08-22 15:02:43 +0100

    update whichuni server to allow ability to get one subject

--- src/whichuni_server.py
+++ src/whichuni_server.py
@@ -1,11 +1,47 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
+from functools import wraps
 from whichunidb import *
 from dbtools import dict_from_row
+from sqlalchemy import func
 
 app = Flask(__name__)
 wudb = WhichUniDB()
 
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'keiran' and password == 'YwRHJ8kB'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
 @app.route('/api/subjects')
+@requires_auth
 def subjects():
     q = dict_from_row(wudb.query(WUSubject) \
             .all())
@@ -14,10 +50,23 @@ def subjects():
         "subjects": [collapse(row)for row in q]
     })
 
+@app.route('/api/subjects/<string:subject_title>')
+@requires_auth
+def subject(subject_title):
+    q = dict_from_row(wudb.query(WUSubject) \
+            .filter(func.lower(subject_title) == func.lower(WUSubject.title)) \
+            .first())
+    if q is None:
+        return not_found()
+    else:
+        return jsonify(collapse(q))
+    return 
+
 def collapse(row):
     row['alevels'] = [a['alevel'] for a in row['alevels']]
     row['careers'] = [c['career'] for c in row['careers']]
     return row
+
 
 if __name__ == "__main__":
     app.run()
