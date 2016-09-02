02b900a91d0a8ccefcb7a12a17d29539baf42e9e
devm1023 <devm1023@gmail.com>
2016-09-02 14:17:21 +0100

    add scripts for whichuni data

--- src/whichuni_fix_data.py
+++ src/whichuni_fix_data.py
@@ -34,8 +34,20 @@ def main():
                     career.title = row[1]
                     print("Changed career {0} to {1}".format(row[0], row[1]))
     
+    def add_careers_to_subject(subject):
+        with open('whichuni/added_careers.csv') as acf:
+            reader = csv.reader(acf)
+            rows = [row for row in reader if row[0].lower() == subject.title.lower()]
+            for row in rows:
+                career = wudb \
+                    .query(WUCareer) \
+                    .filter(func.lower(WUCareer.title) == func.lower(row[1])) \
+                    .first()
+                print(repr(dict_from_row(career)))
+    
     print("Fixing subjects")
     process_db(subjects, fix_subject, wudb, logger=logger) 
+    process_db(subjects, add_careers_to_subject, wudb, logger=logger)
     print("Fixing careers")
     process_db(careers, fix_career, wudb, logger=logger)
 
