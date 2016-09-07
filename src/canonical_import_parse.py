ef2bb942b7f4e7ad317064ae2ef087b5f7e89f35
devm1023 <devm1023@gmail.com>
2016-09-07 14:24:00 +0100

    add first_name and last_name parsing functionality to canonical_import_parse

--- src/canonical_import_parse.py
+++ src/canonical_import_parse.py
@@ -12,6 +12,19 @@ from parse_datetime import parse_datetime
 import re
 import hashlib
 import argparse
+from nameparser import HumanName
+from nameparser.config import CONSTANTS
+
+with open('name_constants.csv', 'r') as inputfile:
+    for line in inputfile:
+        line = line.strip()
+        if not line or line.startswith('#'):
+            continue
+        row = line.split(',')
+        if row[0] == "title":
+            CONSTANTS.titles.add(row[1].strip())
+        if row[0] == "suffix":
+            CONSTANTS.suffix_acronyms.add(row[1].strip())
 
 connectionspatt = re.compile(r'[^0-9]*([0-9]+)[^0-9]*')
 country_languages = {
@@ -20,6 +33,24 @@ country_languages = {
     'Nederland'      : 'nl',
 }
 
+def get_first_name(name):
+    first_name = None
+    try:
+        name = ''.join([i for i in name if i.isalpha() or i == " "])
+        first_name = HumanName(name).first
+    except Exception as e:
+        print(e)
+        pass
+    return first_name
+
+def get_last_name(name):
+    last_name = None
+    try:
+        name = ''.join([i for i in name if i.isalpha() or i == " "])
+        last_name = HumanName(name).last
+    except:
+        pass
+    return last_name
 
 def make_connections(connections):
     if connections is not None:
@@ -77,8 +108,8 @@ def import_liprofiles(jobid, fromid, toid, from_ts, to_ts):
             url           = liprofile.url,
             picture_url   = liprofile.picture_url,
             name          = liprofile.name,
-            last_name     = None,
-            first_name    = None,
+            last_name     = get_last_name(liprofile.name),
+            first_name    = get_first_name(liprofile.name),
             location      = liprofile.location,
             sector        = liprofile.sector,
             title         = liprofile.title,
@@ -129,6 +160,7 @@ def import_liprofiles(jobid, fromid, toid, from_ts, to_ts):
             if not any(bool(groupdict[k]) for k in ['name', 'url']):
                 continue
             profiledict['groups'].append(groupdict)
+        print('{0:s} {1:s}'.format(profiledict['first_name'], profiledict['last_name']))
         cndb.add_liprofile(profiledict)
 
     process_db(q, add_liprofile, cndb, logger=logger)
