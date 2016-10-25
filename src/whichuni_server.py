f595684c28f2c7518219f867c530148ad07bd7aa
devm1023 <devm1023@gmail.com>
2016-10-25 12:34:16 +0100

    update whichuni server

--- src/whichuni_server.py
+++ src/whichuni_server.py
@@ -49,7 +49,7 @@ def courses_at_uni(uni):
                 .join(WUUniversity) \
                 .filter(func.lower(WUUniversity.name) == func.lower(uni)) \
                 .all()
-    return jsonify(dict_from_row(query))
+    return jsonify([collapse_course(q) for q in dict_from_row(query)])
 
 @app.route('/api/courses/<string:uni>/<string:course>')
 @requires_auth
@@ -59,7 +59,7 @@ def courses(uni, course):
                 .filter(func.lower(WUUniversity.name) == func.lower(uni)) \
                 .filter(func.lower(WUCourse.title) == func.lower(course)) \
                 .all()
-    return jsonify(dict_from_row(query))
+    return jsonify([collapse_course(q) for q in dict_from_row(query)])
 
 @app.route('/api/courses/<string:uni>/<string:course>/<string:code>')
 @requires_auth
@@ -70,7 +70,7 @@ def course(uni, course, code):
                 .filter(func.lower(WUCourse.title) == func.lower(course)) \
                 .filter(func.lower(WUCourse.ucas_code) == func.lower(code)) \
                 .first()
-    return jsonify(dict_from_row(query))
+    return jsonify(collapse_course(dict_from_row(query)))
 
 @app.route('/api/universities')
 @requires_auth
@@ -78,7 +78,7 @@ def universities():
     q = dict_from_row(wudb.query(WUUniversity) \
                           .all())
     return jsonify({
-        "universities": [collapse(row) for row in q]
+        "universities": [collapse_university(row) for row in q]
     })
 
 @app.route('/api/universities/<string:university_name>')
@@ -90,7 +90,7 @@ def university(university_name):
     if q is None:
         return not_found()
     else:
-        return jsonify(collapse(dict_from_row(q)))
+        return jsonify(collapse_university(dict_from_row(q)))
     
 @app.route('/api/subjects')
 @requires_auth
@@ -99,7 +99,7 @@ def subjects():
             .all())
         
     return jsonify({
-        "subjects": [collapse(row) for row in q]
+        "subjects": [collapse_subject(row) for row in q]
     })
 
 @app.route('/api/subjects/<string:subject_title>')
@@ -111,7 +111,7 @@ def subject(subject_title):
     if q is None:
         return not_found()
     else:
-        return jsonify(collapse(q))
+        return jsonify(collapse_subject(q))
 
 @app.route('/api/typeahead/careers/<string:partial_career_name>')
 @requires_auth
@@ -151,14 +151,32 @@ def career(career_name):
     else:
         return not_found()
 
-def collapse(row):
-    if 'alevels' in row:
-        row['alevels'] = [a['alevel'] for a in row['alevels']]
-    if 'careers' in row:
-        row['careers'] = [c['career'] for c in row['careers']]
-    if 'university_tags' in row:
-        row['tags'] = [c['tag']['name'] for c in row['university_tags']]
-        del row['university_tags']
+def collapse_course(row):
+    del row['id']
+    del row['university_id']
+    if 'entry_requirements' in row:
+        row['entry_requirements'] = [
+            {
+                "name": entry['entry_requirement']['name'],
+                "grades": entry['grades'],
+                "text": entry['text']
+            } for entry in row['entry_requirements']
+        ]
+    if 'study_types' in row:
+        for s in row['study_types']:
+            del s['course_id']
+            del s['id']
+            s['duration'] = int(s['duration']) if s['duration'] else None
+    if 'university' in row:
+        row['university'] = collapse_university(row['university'])
+    if 'university_subjects' in row:
+        row['university_subjects'] = [collapse_university_subject(s) for s in row['university_subjects']]
+    return row
+
+def collapse_university(row):
+    del row['city']['id']
+    del row['id']
+    del row['city_id']
     if 'university_characteristics' in row:
         row['characteristics'] = [
             { 
@@ -167,9 +185,6 @@ def collapse(row):
                 'score_r': c['score_r']
             } for c in row['university_characteristics']]
         del row['university_characteristics']
-    if 'city' in row:
-        del row['city_id']
-        row['city'] = row['city']['name']
     if 'university_league_tables' in row:
         row['league_tables'] = [
             { 
@@ -179,6 +194,43 @@ def collapse(row):
             } for c in row['university_league_tables']
         ]
         del row['university_league_tables']
+    if 'university_tags' in row:
+        row['tags'] = [c['tag']['name'] for c in row['university_tags']]
+        del row['university_tags']
+    return row
+
+def collapse_university_subject(row):
+    del row['id']
+    del row['course_id']
+    del row['subject_id']
+    for rating in row['ratings']:
+        del rating['id']
+        del rating['university_subject_id']
+    if 'sectors_after' in row:
+        row['sectors_after'] = [
+            {
+                "name": s['sector_after']['name'],
+                "percent": s['percent']
+            } for s in row['sectors_after']
+        ]
+    if 'studied_before' in row:
+        row['studied_before'] = [
+            {
+                "common_grade": s['common_grade'],
+                "common_grade_percent": s['common_grade_percent'],
+                "percent": s['percent'],
+                "name": s['studied_before']['name']
+            } for s in row['studied_before']
+        ]
+    if 'subject' in row:
+        row['subject'] = collapse_subject(row['subject'])
+    return row
+
+def collapse_subject(row):
+    if 'alevels' in row:
+        row['alevels'] = [a['alevel'] for a in row['alevels']]
+    if 'careers' in row:
+        row['careers'] = [c['career'] for c in row['careers']]
     return row
 
 
