2d43bc1e93f9ef3a25fbad6de61c93dda7915a46
devm1023 <devm1023@gmail.com>
2017-11-01 13:44:55 +0000

    Add support for building indeed titles file with unstemmed skills

--- src/build_indeed_job_titles.py
+++ src/build_indeed_job_titles.py
@@ -1,15 +1,22 @@
 import argparse
 import csv
 
+from canonicaldb import CanonicalDB, Entity
 
 """
    Script builds a file to be used by get_indeed_jobs.py as titles input file.
    The script uses a skill vector file as an input. 
 """
 
-
+def skill_name(cndb, nrm_skill):
+    name = cndb.query(Entity.name).filter(Entity.nrm_name == nrm_skill).first()
+    if name is not None:
+        return name[0]
+    return nrm_skill.split(':')[-1]
 
 def main(args):
+    cndb = CanonicalDB()
+
     skill_limit = args.skill_limit
     titles = {}
     with open(args.sv, 'r') as infile:
@@ -23,7 +30,11 @@ def main(args):
             else:
                 if len(skills) > skill_limit:
                     continue
-                skills.append(row[1].split(':')[-1])
+                if args.destem_skills:
+                    skill = skill_name(cndb, row[1])
+                else:
+                    skill = row[1].split(':')[-1]
+                skills.append(skill)
 
     if args.format == 'title':
         with open('indeed_titles_title.txt', 'w') as outputfile:
@@ -51,5 +62,8 @@ if __name__ == '__main__':
     parser.add_argument('--skill_limit', type=int,
                         help='Number of most relevant skills to add. Only applicable with --format skill option.',
                         default=5)
+    parser.add_argument('--destem-skills', type=bool,
+                        help='Remove stemming from skills by looking up names in the Entity table. Only applicable with --format skill option.',
+                        default=False)
     args = parser.parse_args()
     main(args)
