4b0d3c1fc7f46bdb573690b7921718cde0032ab4
devm1023 <devm1023@gmail.com>
2018-01-10 16:10:51 +0000

    API to get top skills for merged tittles.

--- src/jobdata_server.py
+++ src/jobdata_server.py
@@ -4,11 +4,11 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-from sqlalchemy import func
+from sqlalchemy import func, desc
 from sqlalchemy.sql.expression import literal_column
 
 import conf
-from canonicaldb import CanonicalDB, ADZJob, INJob, LA, LEP, LAInLEP
+from canonicaldb import CanonicalDB, ADZJob, ADZJobSkill, INJob, INJobSkill, LA, LEP, LAInLEP
 from dbtools import dict_from_row
 
 # Create application
@@ -17,7 +17,6 @@ CORS(app)
 
 # Create database sessions
 cndb = CanonicalDB()
-
 
 # Configure app and create database view
 app.config['SQLALCHEMY_DATABASE_URI'] = conf.CANONICAL_DB
@@ -144,11 +143,46 @@ def get_mergedtitleskills():
     category = request.args.get('category')
     mergedtitle = request.args.get('mergedtitle')
     region = request.args.get('region')
-    region_type = request.args.get('region', 'la')
+    region_type = request.args.get('regiontype', 'la')
+    limit = request.args.get('limit', 20)
+
+    if not category or not mergedtitle or not region or not region_type:
+        return jsonify({'msg': 'insufficient query arguments', 'status': 'ERROR'})
 
     # build results
     results = {}
     total = 0
+
+    def built_query(jobstable, skillstable ,category, mergedtitle, region, region_type, limit):
+        countcol = func.count().label('counts')
+
+        q = cndb.query(skillstable.name, countcol) \
+            .join(jobstable) \
+            .filter(skillstable.language == 'en') \
+            .filter(jobstable.language == 'en') \
+            .filter(jobstable.category == category) \
+            .filter(jobstable.merged_title == mergedtitle) \
+
+        if region_type == 'la':
+            q = q.filter(jobstable.la_id == region)
+        elif region_type == 'lep':
+            q = q.join(LAInLEP, jobstable.la_id == LAInLEP.la_id) \
+                    .join(LEP, LAInLEP.lep_id == LEP.id) \
+                    .filter(LEP.name == region)
+        elif region_type == 'nuts0':
+            q = q.filter(jobstable.nuts0 == region)
+        elif region_type == 'nuts1':
+            q = q.filter(jobstable.nuts1 == region)
+        elif region_type == 'nuts2':
+            q = q.filter(jobstable.nuts2 == region)
+        elif region_type == 'nuts3':
+            q = q.filter(jobstable.nuts3 == region)
+
+        q = q.group_by(skillstable.name) \
+            .order_by(desc(countcol)) \
+            .limit(limit)
+
+        return q
 
     def build_results(q):
         nonlocal total
@@ -157,37 +191,11 @@ def get_mergedtitleskills():
         if q is None:
             return
 
-        for region_id, region_code, region_name, job_title, count in q:
-            key = region_code
+        for skill_name, count in q:
+            results[skill_name] = count
 
-            if job_title is None:
-                job_title = "unknown"
-
-            # new region
-            if key not in results:
-                results[key] = {}
-                if region_name is not None:
-                    results[key]['name'] = region_name
-                # leps for la
-                if leps is not None:
-                    if region_id in leps:
-                        results[key]['leps'] = leps[region_id]
-                    else:
-                        results[key]['leps'] = []
-                results[key]['count'] = 0
-                results[key]['merged_titles'] = {}
-
-            results[key]['count'] += count
-
-            # add title
-            if job_title not in results[key]['merged_titles']:
-                results[key]['merged_titles'][job_title] = 0
-            results[key]['merged_titles'][job_title] += count
-
-            total += count
-
-    build_results(get_breakdown_for_source(ADZJob, category, titles, region_type))
-    build_results(get_breakdown_for_source(INJob, category, titles, region_type))
+    build_results(built_query(ADZJob, ADZJobSkill, category, mergedtitle, region, region_type, limit))
+    build_results(built_query(INJob, INJobSkill, category, mergedtitle, region, region_type, limit))
 
     end = datetime.now()
     response = jsonify({'results' : results,
@@ -197,8 +205,6 @@ def get_mergedtitleskills():
     print('response sent [{0:s}]' \
           .format(datetime.now().strftime('%d/%b/%Y %H:%M:%S')))
     return response
-
-
 
 
 if __name__ == '__main__':
