7a52b05d9046ff329b60f31c5fabbedc6693dcad
devm1023 <devm1023@gmail.com>
2018-12-13 13:52:38 +0000

    Added Fact and Dimension Tables to database to speed up reporting

--- src/canonicaldb.py
+++ src/canonicaldb.py
@@ -32,6 +32,11 @@ __all__ = [
     'Location',
     'CanonicalDB',
     'SkillsIdf',
+    'ReportFactJobs',
+    'ReportDimCategory',
+    'ReportDimRegionCode',
+    'ReportDimRegionType',
+    'ReportDimDatePeriod',
     ]
 
 import conf
@@ -975,6 +980,43 @@ class SkillsIdf(SQLBase):
     __tablename__ = 'skills_idf'
     name          = Column(Unicode(STR_MAX), index=True, primary_key=True)
     idf           = Column(Float)
+
+# Report Dimensions
+
+class ReportDimCategory(SQLBase):
+    __tablename__ = 'report_dim_category'
+    category_name  = Column(String(STR_MAX), index=True, primary_key=True)
+
+
+class ReportDimRegionCode(SQLBase):
+    __tablename__ = 'report_dim_region_code'
+    region_name   = Column(String(STR_MAX), index=True, primary_key=True)
+    region_type   = Column(String(STR_MAX), index=True, priamry_key=True)
+    region_ref    = Column(String(STR_MAX), index=True, primary_key=True)
+
+
+class ReportDimRegionType(SQLBase):
+    __tablename__ = 'report_dim_region_type'
+    region_type_name =  Column(String(STR_MAX), index=True, primary_key=True)
+
+
+class ReportDimDatePeriod(SQLBase):
+    __tablename__ = 'report_dim_category'
+    period_name   = Column(String(STR_MAX), index=True, primary_key=True)
+    start_date    = Column(DateTime)
+    emd_date      = Column(DateTime)
+
+# ReportFactJobs
+
+class ReportFactJobs(SQLBase):
+    __tablename__ = 'report_fact_jobs'
+    date_period   = Column(String(STR_MAX), index=True, primary_key=True, ForeignKey('report_dim_date_period.period_name'))
+    category      = Column(Unicode(STR_MAX), index=True, primary_key=True, ForeignKey('report_dim_category.category_name'))
+    merged_title  = Column(String(STR_MAX), index=True, primary_key=True)
+    region_code   = Column(String(STR_MAX), index=True, primary_key=True, ForeignKey('report_dim_region_code.region_name'))
+    region_type   = Column(String(STR_MAX), index=True, primary_key=True, ForeignKey('report_dim_region_type.region_type_name'))
+    total_jobs    = Column(Integer)
+
 
 
 def _joinfields(*args):
