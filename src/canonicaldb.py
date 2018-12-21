a9cff1424772e172e19d9ac86bd0e8ba893b82e3
devm1023 <devm1023@gmail.com>
2018-12-21 13:19:19 +0000

    Update Name to Code

--- src/canonicaldb.py
+++ src/canonicaldb.py
@@ -990,7 +990,7 @@ class ReportDimCategory(SQLBase):
 
 class ReportDimRegionCode(SQLBase):
     __tablename__ = 'report_dim_region_code'
-    region_name   = Column(String(1000), primary_key=True)
+    region_code   = Column(String(1000), primary_key=True)
     region_type   = Column(String(20), primary_key=True)
     region_ref    = Column(String(1000), primary_key=True)
 
